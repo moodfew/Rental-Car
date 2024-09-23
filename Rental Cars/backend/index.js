5af2ceb9083bb2451a75e838c432a26343e40c89
Denis <rrapajdenis2006@gmail.com>
2024-09-23 16:17:26 +0200

    Express session and LocalStrategy

--- Rental Cars/backend/index.js
+++ Rental Cars/backend/index.js
@@ -1,12 +1,49 @@
 import express from "express";
 import cors from "cors";
 import { check, validationResult } from "express-validator";
+import pg from "pg";
+import session from "express-session";
+import passport, { use } from "passport";
+import { Strategy as LocalStrategy } from "passport-local";
+import bcrypt from "bcrypt";
 
 const app = express();
 const port = 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use(
+  session({
+    secret: "reverend-insanity",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await db.query("SELECT * FROM users WHERE id = $1", [id]);
+    done(null, user.rows[0]);
+  } catch (err) {
+    done(err);
+  }
+});
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "Rental Cars",
+  password: "post123gres",
+  port: 5432,
+});
+
+db.connect();
+
+passport.serializeUser;
 
 app.post(
   "/user/register",
@@ -28,6 +65,32 @@ app.post(
     console.log(email, name, password);
     res.json({ message: "User registered successfully", email, name });
   }
+);
+
+passport.use(
+  new LocalStrategy(
+    { usernameField: "email" },
+    async (email, password, done) => {
+      try {
+        const user = await db.query("SELECT * FROM users WHERE email = $1", [
+          email,
+        ]);
+
+        if (user.rows.length === 0) {
+          return done(null, false, { message: "Now user with that email" });
+        }
+
+        const isMatch = bcrypt.compare(passport, user.rows[0].password);
+
+        if (isMatch) {
+          return done(null, false, { message: "Password incorrect" });
+        }
+        return done(null, user.rows[0]);
+      } catch (err) {
+        return done(err);
+      }
+    }
+  )
 );
 
 app.listen(port, () => {
--- Rental Cars/backend/queries.sql
+++ Rental Cars/backend/queries.sql
@@ -38,6 +38,17 @@ CREATE TABLE locations (
 
 CREATE TABLE car_locations (
 	car_id INT REFERENCES cars(id) ON DELETE CASCADE,
-	location_id INT REFERENCES locations(id) ON DELETE CASCADE
-	
+	location_id INT REFERENCES locations(id) ON DELETE CASCADE,
+	PRIMARY KEY (car_id, location_id)
 );
+
+CREATE TABLE payments (
+	id SERIAL PRIMARY KEY,
+	rental_id INT REFERENCES rentals(id) ON DELETE CASCADE,
+	amount DECIMAL(10,2) NOT NULL,
+	payment_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	status VARCHAR(55) DEFAULT 'paid5'
+);
+
+INSERT INTO users (name, email, password, role)
+VALUES ('Test', 'test@test.com', 'test1234', 'customer');
